feddf8872149347ee514c4e7c1d248bfaccd2e7f
Junyu Liu <liujunyu347@gmail.com>
2020-07-05 00:26:38 -0700

    add positive and nagative test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,13 +6,19 @@ import static org.junit.Assert.*;
 public class CalculatorTest {
 
     @Test
-    public void testcase()
+    public void positive()
     {
         Calculator c = new Calculator();
         assertEquals( 3, c.add(1,2) );
         assertEquals( 4, c.minus(6,2) );
         assertEquals( 12, c.multiply(6,2) );
         assertEquals( 12, c.divide(24,2) );
+    }
+
+    @Test
+    public void negative()
+    {
+        Calculator c = new Calculator();
         assertNotEquals(3, c.add(3,3));
         assertNotEquals(6, c.minus(4,2));
     }
